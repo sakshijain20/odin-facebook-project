45e17fd58e133045357ee38464d956e5921364f3
Sakshi Madrecha <38380683+sakshijain20@users.noreply.github.com>
2021-02-16 22:32:40 +0530

    Delete environment.rb

--- config/environment.rb
+++
@@ -1,7 +0,0 @@
-# Load the Rails application.
-require_relative "application"
-
-# Initialize the Rails application.
-Rails.application.initialize!
-FACEBOOK_APP_ID=752338259015003
-FACEBOOK_SECRET='ec29ad11b66d775230ee240b5a67d960'
